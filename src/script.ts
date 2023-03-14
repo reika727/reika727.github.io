d22c2fbfc95230bb2560488dc75c248cd171c421
reika727 <reikasuya727@gmail.com>
2023-03-15 02:52:03 +0900

    move createGradient into drawingProperty

--- src/script.ts
+++ src/script.ts
@@ -28,19 +28,6 @@ const canvasWidthRatioToRotorRadius = enigma.rotors.length * boundingSquareSideR
 /* ロータの基準円半径に対するキャンバスの高さの比 */
 const canvasHeightRatioToRotorRadius = 2 * boundingSquareSideRatioToRotorRadius + 3 * paddingRatioToRotorRadius;
 
-const canvas = document.getElementById('myCanvas') as HTMLCanvasElement;
-
-window.onload = window.onresize = () => {
-    const canvasWidthRatioToHeight = canvasWidthRatioToRotorRadius / canvasHeightRatioToRotorRadius;
-    /* 幅が最大で画面の 95%、かつ高さが最大で画面の 2/3 を満たしつつなるべく大きくする */
-    const maxWidth = document.documentElement.clientWidth * .95;
-    const maxHeight = document.documentElement.clientHeight * (2 / 3);
-    [canvas.width, canvas.height] =
-        maxWidth / canvasWidthRatioToHeight <= maxHeight
-            ? [maxWidth, maxWidth / canvasWidthRatioToHeight]
-            : [maxHeight * canvasWidthRatioToHeight, maxHeight];
-};
-
 function getDrawingProperty(canvas: HTMLCanvasElement) {
     const rotorRadius = canvas.width / canvasWidthRatioToRotorRadius;
     const rotorInternalRadius = rotorRadius * 0.7; /* HACK: 0.7 という倍率に深い意味はない */
@@ -68,17 +55,18 @@ function getDrawingProperty(canvas: HTMLCanvasElement) {
         getAbsoluteHoleCoord: (n: number, c: {x: number, y: number}, inOut: 'in' | 'out') => ({
             x: (inOut == 'in' ? rotorInternalRadius : rotorRadius) * Math.cos(2 * Math.PI / enigma.alphabet.size * n - Math.PI / 2) + c.x,
             y: (inOut == 'in' ? rotorInternalRadius : rotorRadius) * Math.sin(2 * Math.PI / enigma.alphabet.size * n - Math.PI / 2) + c.y
-        })
+        }),
+        createGradient: (c0: {x: number, y: number}, c1: {x: number, y: number}, lineNumber: number) => {
+            const context = canvas.getContext('2d');
+            assert(context);
+            const linesCount = 4 * enigma.rotors.length + 5;
+            const lineargradient = context.createLinearGradient(c0.x, c0.y, c1.x, c1.y);
+            lineargradient.addColorStop(0, `hsl(${lineNumber / linesCount}turn, 100%, 50%)`);
+            lineargradient.addColorStop(1, `hsl(${(lineNumber + 1) / linesCount}turn, 100%, 50%)`);
+            return lineargradient;
+        }
     };
 };
-
-function createGradient(context: CanvasRenderingContext2D, x0: number, y0: number, x1: number, y1: number, lineNumber: number) {
-    const linesCount = 4 * enigma.rotors.length + 5;
-    const lineargradient = context.createLinearGradient(x0, y0, x1, y1);
-    lineargradient.addColorStop(0, `hsl(${lineNumber / linesCount}turn, 100%, 50%)`);
-    lineargradient.addColorStop(1, `hsl(${(lineNumber + 1) / linesCount}turn, 100%, 50%)`);
-    return lineargradient;
-}
 
 function drawEnigma(canvas: HTMLCanvasElement, enigma: AbstractEnigma, pathChar: string | null = null) {
     const context = canvas.getContext('2d');
@@ -98,8 +86,8 @@ function drawEnigma(canvas: HTMLCanvasElement, enigma: AbstractEnigma, pathChar:
         context.moveTo(from.x, from.y);
         context.lineTo(to.x, to.y);
         [context.strokeStyle, context.lineWidth] =
-            exchangee == path?.exchangedIn ? [createGradient(context, from.x, from.y, to.x, to.y, 0), dp.pathWidth] /* 0 */
-            : exchangee == path?.rotorsOut[path.rotorsOut.length - 1] ? [createGradient(context, to.x, to.y, from.x, from.y, 4 * enigma.rotors.length + 4), dp.pathWidth] /* 4 * |rotors| + 4 */
+            exchangee == path?.exchangedIn ? [dp.createGradient(from, to, 0), dp.pathWidth] /* 0 */
+            : exchangee == path?.rotorsOut[path.rotorsOut.length - 1] ? [dp.createGradient(to, from, 4 * enigma.rotors.length + 4), dp.pathWidth] /* 4 * |rotors| + 4 */
             : exchangee == i ? ['lightgray', 1]
             : ['black', 1];
         context.stroke();
@@ -110,8 +98,8 @@ function drawEnigma(canvas: HTMLCanvasElement, enigma: AbstractEnigma, pathChar:
         context.moveTo(to.x, to.y);
         context.lineTo(nextRotorCoord.x, nextRotorCoord.y);
         [context.strokeStyle, context.lineWidth] =
-            exchangee == path?.exchangedIn ? [createGradient(context, to.x, to.y, nextRotorCoord.x, nextRotorCoord.y, 1), dp.pathWidth] /* 1 */
-            : exchangee == path?.rotorsOut[path.rotorsOut.length - 1] ? [createGradient(context, nextRotorCoord.x, nextRotorCoord.y, to.x, to.y, 4 * enigma.rotors.length + 3), dp.pathWidth] /* 4 * |rotors| + 3 */
+            exchangee == path?.exchangedIn ? [dp.createGradient(to, nextRotorCoord, 1), dp.pathWidth] /* 1 */
+            : exchangee == path?.rotorsOut[path.rotorsOut.length - 1] ? [dp.createGradient(nextRotorCoord, to, 4 * enigma.rotors.length + 3), dp.pathWidth] /* 4 * |rotors| + 3 */
             : ['lightgray', 1];
         context.stroke();
         context.lineWidth = 1;
@@ -141,8 +129,8 @@ function drawEnigma(canvas: HTMLCanvasElement, enigma: AbstractEnigma, pathChar:
             context.moveTo(holeFrom.x, holeFrom.y);
             context.lineTo(holeTo.x, holeTo.y);
             [context.strokeStyle, context.lineWidth] =
-                rotor.passInward(j) == path?.rotorsIn[i] ? [createGradient(context, holeFrom.x, holeFrom.y, holeTo.x, holeTo.y, 2 * i + 2), dp.pathWidth] /* 2, 4, ... , 2 * |rotors| */
-                : j == path?.rotorsOut[enigma.rotors.length - 1 - i] ? [createGradient(context, holeTo.x, holeTo.y, holeFrom.x, holeFrom.y, 4 * enigma.rotors.length - 2 * i + 2), dp.pathWidth] /* 4 * |rotors| + 2, 4 * |rotors|, ... , 2 * |rotors| + 4 */
+                rotor.passInward(j) == path?.rotorsIn[i] ? [dp.createGradient(holeFrom, holeTo, 2 * i + 2), dp.pathWidth] /* 2, 4, ... , 2 * |rotors| */
+                : j == path?.rotorsOut[enigma.rotors.length - 1 - i] ? [dp.createGradient(holeTo, holeFrom, 4 * enigma.rotors.length - 2 * i + 2), dp.pathWidth] /* 4 * |rotors| + 2, 4 * |rotors|, ... , 2 * |rotors| + 4 */
                 : ['black', 1];
             context.stroke();
             context.lineWidth = 1;
@@ -156,8 +144,8 @@ function drawEnigma(canvas: HTMLCanvasElement, enigma: AbstractEnigma, pathChar:
             );
             context.lineTo(nextHoleCoord.x, nextHoleCoord.y);
             [context.strokeStyle, context.lineWidth] =
-                rotor.passInward(j) == path?.rotorsIn[i] ? [createGradient(context, holeTo.x, holeTo.y, nextHoleCoord.x, nextHoleCoord.y, 2 * i + 3), dp.pathWidth] /* 3, 5, ... , |rotors| * 2 + 1 */
-                : j == path?.rotorsOut[enigma.rotors.length - 1 - i] ? [createGradient(context, nextHoleCoord.x, nextHoleCoord.y, holeTo.x, holeTo.y, 4 * enigma.rotors.length - 2 * i + 1), dp.pathWidth] /* 4 * |rotors| + 1, 4 * |rotors| - 1, ... , 2 * |rotors| + 3 */
+                rotor.passInward(j) == path?.rotorsIn[i] ? [dp.createGradient(holeTo, nextHoleCoord, 2 * i + 3), dp.pathWidth] /* 3, 5, ... , |rotors| * 2 + 1 */
+                : j == path?.rotorsOut[enigma.rotors.length - 1 - i] ? [dp.createGradient(nextHoleCoord, holeTo, 4 * enigma.rotors.length - 2 * i + 1), dp.pathWidth] /* 4 * |rotors| + 1, 4 * |rotors| - 1, ... , 2 * |rotors| + 3 */
                 : ['lightgray', 1];
             context.stroke();
             context.lineWidth = 1;
@@ -181,14 +169,27 @@ function drawEnigma(canvas: HTMLCanvasElement, enigma: AbstractEnigma, pathChar:
         context.moveTo(holeFrom.x, holeFrom.y);
         context.lineTo(holeTo.x, holeTo.y);
         [context.strokeStyle, context.lineWidth] =
-            enigma.reflector.pass(i) == path?.reflected ? [createGradient(context, holeFrom.x, holeFrom.y, holeTo.x, holeTo.y, 2 * enigma.rotors.length + 2), dp.pathWidth] /* 2 * |rotors| + 2 */
-            : i == path?.reflected ? [createGradient(context, holeTo.x, holeTo.y, holeFrom.x, holeFrom.y, 2 * enigma.rotors.length + 2), dp.pathWidth] /* 2 * |rotors| + 2 */
+            enigma.reflector.pass(i) == path?.reflected ? [dp.createGradient(holeFrom, holeTo, 2 * enigma.rotors.length + 2), dp.pathWidth] /* 2 * |rotors| + 2 */
+            : i == path?.reflected ? [dp.createGradient(holeTo, holeFrom, 2 * enigma.rotors.length + 2), dp.pathWidth] /* 2 * |rotors| + 2 */
             : ['black', 1];
         context.stroke();
         context.lineWidth = 1;
         drawns.push(enigma.reflector.pass(i));
     }
 }
+
+const canvas = document.getElementById('myCanvas') as HTMLCanvasElement;
+
+window.onload = window.onresize = () => {
+    const canvasWidthRatioToHeight = canvasWidthRatioToRotorRadius / canvasHeightRatioToRotorRadius;
+    /* 幅が最大で画面の 95%、かつ高さが最大で画面の 2/3 を満たしつつなるべく大きくする */
+    const maxWidth = document.documentElement.clientWidth * .95;
+    const maxHeight = document.documentElement.clientHeight * (2 / 3);
+    [canvas.width, canvas.height] =
+        maxWidth / canvasWidthRatioToHeight <= maxHeight
+            ? [maxWidth, maxWidth / canvasWidthRatioToHeight]
+            : [maxHeight * canvasWidthRatioToHeight, maxHeight];
+};
 
 setInterval(() => {
     const char = enigma.alphabet.at(Math.floor(Math.random() * enigma.alphabet.size));
